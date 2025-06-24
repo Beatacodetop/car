8174becaa4b28ccd322261a74cfa1eedbc66acc4
Beatacodetop <beata9sidor@gmail.com>
2025-06-24 11:08:15 +0200

    wip: ramka lista Wy

--- notatnik.py
+++ notatnik.py
@@ -1,10 +1,19 @@
 from tkinter import *
-import tkintermapview
+import  tkintermapview
 
 
 root = Tk()
 root.title('Wypożyczalnie')
 root.geometry('1500x1000')
+
+ramka_lista_wypozyczalni = Frame(root)
+
+ramka_lista_wypozyczalni.grid(row=0, column=0, columnspan=1 )
+label_lista_wypozyczalni = Label(ramka_lista_wypozyczalni, text='Lista Wypożyczalni')
+label_lista_wypozyczalni.grid(row=1, column=0)
+
+listbox_lista_wypozyczalni = Listbox(ramka_lista_wypozyczalni)
+listbox_lista_wypozyczalni.grid(row=0, column=0, columnspan=1)
 
 
 root.mainloop()
