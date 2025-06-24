a82baf8d3548a305cf1f57169dc1dd833af7b611
Beatacodetop <beata9sidor@gmail.com>
2025-06-24 11:25:18 +0200

    wip: formularz kli

--- notatnik.py
+++ notatnik.py
@@ -62,9 +62,33 @@ ramka_lista_klientow = Frame(root)
 ramka_lista_klientow.grid(row=0, column=2, columnspan=1)
 label_lista_klientow = Label(ramka_lista_klientow  , text='Lista Klientów')
 label_lista_klientow.grid(row=1, column=0)
-listbox_lista_kLientow = Listbox(ramka_lista_klientow)
-listbox_lista_kLientow.grid(row=0, column=0, columnspan=1)
+listbox_lista_klientow = Listbox(ramka_lista_klientow)
+listbox_lista_klientow.grid(row=0, column=0, columnspan=1)
 
 
+# formularz_klienta
+ramka_formularz_klientow = Frame(root)
+ramka_formularz_klientow.grid(row=1, column=2, columnspan=1)
+
+label_formularz_klientow = Label(ramka_formularz_klientow, text='Formularz Klienta')
+label_formularz_klientow.grid(row=0, column=1, columnspan=2)
+
+label_imie = Label(ramka_formularz_klientow, text='Imie')
+label_imie.grid(row=1, column=2, sticky=W)
+
+label_nazwisko = Label(ramka_formularz_klientow, text='Nazwisko')
+label_nazwisko.grid(row=2, column=2, sticky=W)
+
+label_adres = Label(ramka_formularz_klientow, text='Adres:')
+label_adres.grid(row=3, column=2, sticky=W)
+
+label_pesel = Label(ramka_formularz_klientow, text='PESEL:')
+label_pesel.grid(row=4, column=2, sticky=W)
+
+label_auto = Label(ramka_formularz_klientow, text='Auto:')
+label_auto.grid(row=5, column=2, sticky=W)
+
+label_wypozyzalnia = Label(ramka_formularz_klientow, text='Wypożyczalnia:')
+label_wypozyzalnia.grid(row=6, column=2, sticky=W)
 
 root.mainloop()
