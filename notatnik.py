b8bdd81eaf3c8b079255a3c0086b3b567ae11c78
Beatacodetop <beata9sidor@gmail.com>
2025-06-24 11:10:45 +0200

    wip: ramka lista Pra

--- notatnik.py
+++ notatnik.py
@@ -9,11 +9,25 @@ root.geometry('1500x1000')
 ramka_lista_wypozyczalni = Frame(root)
 
 ramka_lista_wypozyczalni.grid(row=0, column=0, columnspan=1 )
+
 label_lista_wypozyczalni = Label(ramka_lista_wypozyczalni, text='Lista Wypożyczalni')
 label_lista_wypozyczalni.grid(row=1, column=0)
 
 listbox_lista_wypozyczalni = Listbox(ramka_lista_wypozyczalni)
 listbox_lista_wypozyczalni.grid(row=0, column=0, columnspan=1)
+
+
+
+ramka_lista_Pracowników = Frame(root)
+
+ramka_lista_Pracowników.grid(row=0, column=1, columnspan=1)
+
+label_lista_Pracownikow = Label(ramka_lista_Pracowników, text='Lista Pracowników')
+label_lista_Pracownikow.grid(row=1, column=0)
+
+listbox_lista_Pracownikow = Listbox(ramka_lista_Pracowników)
+listbox_lista_Pracownikow.grid(row=0, column=0, columnspan=1)
+
 
 
 root.mainloop()
